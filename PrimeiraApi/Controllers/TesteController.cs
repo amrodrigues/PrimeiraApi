b628e69bb60c51f805e0699c904951b79a9cf04b
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-06-16 19:33:18 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraApi/Controllers/TesteController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraApi.Controllers
+{
+    [ApiController]
+    [Route("api/demo")]
+    public class TesteController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok(new Produto { Id = 1, Nome = "Arroz", Preco = 23, Quantidade = 1 });
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Produto { Id = 1, Nome = "Arroz", Preco = 23, Quantidade = 1 });
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Post([FromBody] Produto produto)
+        {
+            if (produto == null)
+            {
+                return BadRequest("Produto não pode ser nulo");
+            }
+
+            // Simular a adição do produto
+            //return CreatedAtAction(nameof(Get), new { id = produto.Id }, produto);
+            return CreatedAtAction("Get", new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put(int id, Produto produto)
+        {
+            if (produto == null || produto.Id != id)
+            {
+                return BadRequest("Produto inválido");
+            }
+
+            // Simular a atualização do produto
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            // Simular a exclusão do produto
+            return NoContent();
+        }
+    }
+}
---
+++ PrimeiraApi/Produto.cs
@@ -0,0 +1,22 @@
+﻿namespace PrimeiraApi
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(int id, string nome, decimal preco, int quantidade)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto()
+        {
+        }
+    }
+}
